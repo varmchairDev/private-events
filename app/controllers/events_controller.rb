9a98a9b1b26ea966d8d051ed98e9fa2d69e12ef5
Onur Kaya <onurkaya8@outlook.com>
2020-03-07 18:39:43 +0300

    working on attendance and invitation forms.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
 
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :destroy, 
+                                   :attend_event, :cancel_event, :send_invitation]
   before_action :only_users
   before_action :auth_check, only: [:edit, :update, :destroy] #aplication_helper.rb
 
@@ -50,12 +51,29 @@ class EventsController < ApplicationController
   end
 
   def attend_event
+    redirect_back(fallback_location: root_path) unless !@event.attendees.include?(current_user)
+    @event.attendees << current_user
+    redirect_back(fallback_location: root_path)
   end
 
   def cancel_event
+    redirect_back(fallback_location: root_path) unless @event.attendees.include?(current_user)
+    @event.attendees.delete(current_user)
+    redirect_back(fallback_location: root_path) 
   end
 
   def send_invitation
+    redirect_back(fallback_location: root_path) unless current_user.name != params[:invite][:user_name]
+    invited = User.find_by(name: params[:invite][:user_name])
+    redirect_back(fallback_location: root_path) unless 
+                         Invitation.find_by(inviter_id: current_user.id, invited_id: invited.id, event_id: @event.id).nil?
+    new_invitation = Invitation.new(inviter_id: current_user.id, invited_id: invited.id, event_id: @event.id)
+    if new_invitation.save
+      flash[:success] = "Invitations sended."
+    else 
+      flash[:error] = "Failed to send invitation."
+    end
+    redirect_back(fallback_location: root_path)
   end
 
   private
